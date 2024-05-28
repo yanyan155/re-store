646cf1617ba339e5e3c8921b7834c04533d3295b
yanyan155 <jm@tut.by>
2024-05-28 16:35:32 +0300

    Updates

---
+++ precache-manifest.441613e1b53d3e0ef94823655260522a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15388f2a85ab461c05f4",
+    "url": "/re-store/static/css/main.16c947b4.chunk.css"
+  },
+  {
+    "revision": "15388f2a85ab461c05f4",
+    "url": "/re-store/static/js/main.15388f2a.chunk.js"
+  },
+  {
+    "revision": "fdd3b250603ad237dc3f",
+    "url": "/re-store/static/js/1.fdd3b250.chunk.js"
+  },
+  {
+    "revision": "b21ed876d63c47d8ad40",
+    "url": "/re-store/static/js/runtime~main.b21ed876.js"
+  },
+  {
+    "revision": "74b39d0cccb43bfb9b2427f6ce4eb719",
+    "url": "/re-store/index.html"
+  }
+];
